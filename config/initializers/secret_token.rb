b11e83b4d07721e2ac279a8fd84c7f857ee0bc69
wes <wesley.williams@sabre.com>
2010-07-27 19:50:48 +0800

    created new project for ruby retro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RRetro::Application.config.secret_token = '0359ab766a18652ec8753a6ef6ce9b709a91a63465090c340d521e221e512aa7cc037b9edfd882eafaf2d710d1bef51483dfd926edaa77b26a0d92f9cb987975'
