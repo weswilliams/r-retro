a9ecf15a754c4e598186c58a4e00e53b2b73a552
wes <wesley.williams@sabre.com>
2010-07-31 17:20:49 +0800

    some clean up of unneeded method parameters

--- app/helpers/retrospectives_helper.rb
+++ app/helpers/retrospectives_helper.rb
@@ -1,19 +1,19 @@
 module RetrospectivesHelper
-  def calculate_section_per_row(retrospective)
-    retrospective.sections.length <= 3 ? 3 : 2
+  def calculate_section_per_row()
+    @retrospective.sections.length <= 3 ? 3 : 2
   end
 
-  def is_starting_row(index, retrospective)
-    index == 0 || (index % calculate_section_per_row(retrospective) == 0)
+  def is_starting_row(index)
+    index == 0 || (index % calculate_section_per_row == 0)
   end
 
-  def is_last_section(index, retrospective)
-    index == retrospective.sections.length - 1
+  def is_last_section(index)
+    index == @retrospective.sections.length - 1
   end
 
-  def is_ending_row(index, retrospective)
-    sections_per_row = calculate_section_per_row(retrospective)
-    is_last_section(index, retrospective) ||
+  def is_ending_row(index)
+    sections_per_row = calculate_section_per_row
+    is_last_section(index) ||
       (index < sections_per_row && sections_per_row - index == 1) ||
       (index > sections_per_row && index % sections_per_row == 1)
 
