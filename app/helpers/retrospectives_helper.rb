b10b17e13eec625cbab215aef926bd031ba2ea30
wes <wesley.williams@sabre.com>
2010-08-01 22:19:13 +0800

    - method to click a link periodically

--- app/helpers/retrospectives_helper.rb
+++ app/helpers/retrospectives_helper.rb
@@ -1,5 +1,14 @@
 module RetrospectivesHelper
 
+#  <%= periodically_click_link(:link_to_click => "section_items_#{current_section.id}") %>
+  
+  def periodically_click_link(options = {})
+    frequency = options[:frequency] || 10 # every ten seconds by default
+    link_to_click = options[:link_to_click]
+    code = "new PeriodicalExecuter(function() {document.getElementById('#{link_to_click}').click()}, #{frequency})"
+    javascript_tag(code)
+  end
+
   def beginning_row_index_for_ending_index(ending_row_index)
     return 0 if is_in_first_row(ending_row_index)
     is_single_continuing_row_and_last_section(ending_row_index) ?
@@ -14,7 +23,7 @@ module RetrospectivesHelper
   def number_of_item_columns_for(index)
     is_last_section(index) ? 1 : calculate_sections_per_row
   end
-  
+
   def calculate_sections_per_row()
     @retrospective.sections.length <= 3 ? 3 : 2
   end
@@ -47,10 +56,9 @@ module RetrospectivesHelper
   def is_ending_row(index)
     sections_per_row = calculate_sections_per_row
     is_last_section(index) ||
-      is_first_ending_row(index) ||
-      is_continuing_ending_row(index)
+            is_first_ending_row(index) ||
+            is_continuing_ending_row(index)
   end
-
 
 
 end
