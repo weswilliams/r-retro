ffd4095095d4fa6a6aa6e49838552b8e8480c843
Wes Williams <wesandevie@gmail.com>
2010-10-05 15:08:53 +0800

    added links to create a new item in drag drop assistance text

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function fireOnclick(objID) {
+var target=document.getElementById(objID);
+if(document.dispatchEvent) { // W3C
+    var oEvent = document.createEvent( "MouseEvents" );
+    oEvent.initMouseEvent("click", true, true,window, 1, 1, 1, 1, 1, false, false, false, false, 0, target);
+    target.dispatchEvent( oEvent );
+    }
+else if(document.fireEvent) { // IE
+    target.fireEvent("onclick");
+    }
+}
