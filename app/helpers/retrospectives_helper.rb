69331295f5a18e1617fa043e2c9b22febd7ec9eb
Wes Williams <wesandevie@gmail.com>
2010-10-13 11:34:58 -0500

    look up themes based on css file names

--- app/helpers/retrospectives_helper.rb
+++ app/helpers/retrospectives_helper.rb
@@ -1,8 +1,17 @@
 module RetrospectivesHelper
 
+  def theme_options
+    @files = Dir.glob("public/stylesheets/palettes/*")
+    theme_name = []
+    @files.each do |f|
+      theme_name << /\A.*scaffold-(.*).css\z/.match(f)[1]
+    end
+    theme_name
+  end
+
   def section_rows(sections_per_row = 2)
     rows = []
-    @retrospective.sections.each_slice(sections_per_row) {|row| rows.push row}
+    @retrospective.sections.each_slice(sections_per_row) { |row| rows.push row }
     rows
   end
 
@@ -17,8 +26,9 @@ module RetrospectivesHelper
     eos
     javascript_tag(code)
   end
+
 #  $("window_#{element_id}_content").style.background="##{color}";
-  
+
   def inline_edit_item_with_remote_update(options = [])
     id = options[:id]
     url = options[:url]
@@ -48,10 +58,10 @@ module RetrospectivesHelper
   # could not find this method in rails 3 prototype helper, not sure why, cannot find a reason
   #<%= periodically_call_remote(:url => refresh_retrospective_url(:section_id => section), :frequency => 20) %>
   def periodically_call_remote(options = {})
-     frequency = options[:frequency] || 10 # every ten seconds by default
-     condition = options[:condition] || true
-     code = "new PeriodicalExecuter(function() {if (#{condition}) #{remote_function(options)}}, #{frequency})"
-     javascript_tag(code)
-   end
+    frequency = options[:frequency] || 10 # every ten seconds by default
+    condition = options[:condition] || true
+    code = "new PeriodicalExecuter(function() {if (#{condition}) #{remote_function(options)}}, #{frequency})"
+    javascript_tag(code)
+  end
 
 end
