a604e35636ab1b04325f9eae11736aa11987a3b8
weswilliams <wes.williamz@gmail.com>
2011-02-04 19:15:59 -0600

    - moved item inline add the ujs

--- app/helpers/retrospectives_helper.rb
+++ app/helpers/retrospectives_helper.rb
@@ -15,30 +15,4 @@ module RetrospectivesHelper
     rows
   end
 
-  def inline_edit_item_with_remote_update(options = [])
-    id = options[:id]
-    url = options[:url]
-    refresh_url = options[:refresh_url] || ''
-    element_id = options[:element_id] || 'add_item'
-    on_complete = options[:on_complete] || ", onComplete: function(transport, element) {#{remote_function(:url => refresh_url)};}"
-    rows = options[:rows] || '1'
-
-    code = <<-eos
-    var #{element_id}_editor#{id} = new Ajax.InPlaceEditor('inline_#{element_id}_#{id}',
-            '#{url}', {
-      externalControl:"inline_#{element_id}_#{id}",
-      highlightcolor: 'transparent',
-      rows: #{rows},
-      cancelText: '(cancel)',
-      okText: '(ok)',
-      clickToEditText: 'double click to edit'#{on_complete}
-    });
-    $("inline_#{element_id}_#{id}").ondblclick = function() {
-      #{element_id}_editor#{id}.enterEditMode();
-    }
-    #{element_id}_editor#{id}.dispose();
-    eos
-    javascript_tag(code)
-  end
-
 end
